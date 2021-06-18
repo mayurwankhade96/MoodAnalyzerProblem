cb0b08bba5236fcffcd66d729f626eea8a6069b2
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-18 21:25:42 +0530

    [Mayur] Add : Welcome message added.

---
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problem!");
+        }
+    }
+}
